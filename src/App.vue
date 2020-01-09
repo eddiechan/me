ff022f994040c75eb8c1444d1c93d6379d49fd17
Chan, Eddie C <eddie.c.chan@sap.com>
2020-01-08 18:46:29 -0800

    * Using Google Fonts (the simple way...) * Simple page using CSS Grid.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="grid-container">
+    <Sidemenu msg="The personal website of Eddie Chan. Under construction." class="sidemenu"/>
+    <HelloWorld msg="Muji!" class="main"/>
   </div>
 </template>
 
 <script>
+import Sidemenu from './components/Sidemenu.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sidemenu, HelloWorld
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Acme');
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-areas:
+    'sidemenu main';
+  grid-gap: 0px;
+  background-color: #FFF;
+  padding: 0px;
+}
+
+.sidemenu {
+  grid-area: sidemenu;
+  background-color: teal;
+  width: 200px;
+}
+
+.main {
+  grid-area: main;
+  text-align: left;
+  background-color: pink;
 }
 </style>
